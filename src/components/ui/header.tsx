5444f464a3d0269500adb6d0b198149956ade0ba
Justin Spidell <justintspidell@gmail.com>
2025-02-24 17:00:28 -0800

    fixed header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,33 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header>
-      <ModeToggle />
+    <header className="min-w-full flex flex-row justify-around p-4 border-b-2 border-neutral-500 border-dashed">
+      <div className="basis-2/3">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/experience" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Experience
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div className="">
+        <ModeToggle />
+      </div>
     </header>
   );
 }
